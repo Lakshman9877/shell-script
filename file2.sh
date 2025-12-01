f470b357a83f7502e2387cdf2efdb987a61f4b94
lakshman <lakshman>
2025-12-01 12:26:08 +0530

    scripts

---
+++ file2.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo "Please enter your username"
+
+read USERNAME 
+
+echo "username enterd is $USERNAME"
+
+echo "please enter password"
+
+read -s Password
+
+echo " password entered is $Password"
