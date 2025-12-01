5687976ce715226923b0849967fb8ea581e39e99
lakshman <lakshman>
2025-12-01 11:36:22 +0530

    scripts

--- variable.sh
+++ variable.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 
-echo "ram:Hi raghu, Good Morning!"
-echo "raghu:Good Morning, How are you"
-echo "ram: Good, what about you"
+person1=ram
+person2=raghu
+
+
+echo "$person1:Hi raghu, Good Morning!"
+echo "$person2:Good Morning, How are you"
+echo "$person1: Good, what about you"
